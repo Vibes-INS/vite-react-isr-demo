3aa49c1c69e56f33012fd18245a10236517f56fc
INS <j893412899@outlook.com>
2022-12-22 20:34:53 +0800

    to: testing isr

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import ssr from 'vite-plugin-ssr/plugin';
 import vercel from 'vite-plugin-vercel';
 import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    ssr({
-      prerender: {
-        noExtraDir: false,
-        // partial: true,
-      },
-    }),
-    vercel(),
-    vercelSsr(),
-  ],
-  vercel: {
-    expiration: 25,
-    isr: {
-      '/about': { expiration: 15 },
+export default defineConfig(async () => {
+  return {
+    plugins: [
+      react(),
+      ssr({
+        prerender: {
+          noExtraDir: true,
+        },
+      }),
+      vercel(),
+      vercelSsr(),
+    ],
+    vercel: {
+      expiration: 25,
     },
-  },
-  server: {
-    port: 3000,
-  },
-})
+    server: {
+      port: 3000,
+    },
+  };
+});
