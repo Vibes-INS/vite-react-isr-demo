7569843316790eac4d371f045d169306ba71e529
INS <j893412899@outlook.com>
2022-12-22 16:01:54 +0800

    to: testing isr

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin';
 import vercel from 'vite-plugin-vercel';
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 
 export default defineConfig({
   plugins: [
@@ -12,12 +13,13 @@ export default defineConfig({
       },
     }),
     vercel(),
+    // vercelSsr(),
   ],
   vercel: {
     expiration: 25,
-    isr: {
-      '/about': { expiration: 15, symlink: 'ssr_', route: '^/about$' },
-    }
+    // isr: {
+    //   '/about': { expiration: 15, symlink: 'isr_', route: '^/about$' },
+    // },
   },
   server: {
     port: 3000,
